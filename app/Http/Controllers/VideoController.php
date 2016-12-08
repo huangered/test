cd38cada8ee7a9d4ef49c19213e5a9bfde500d66
peter <huangered@hotmail.com>
2016-12-08 05:43:06 -0500

    c2

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -9,7 +9,7 @@ class VideoController extends Controller
     //
     public function index()
     {
-	$video = "video/os_simpsons_s25e22_720p.mp4";
+	$video = "video/demo.mp4";
 	$mime = "video/mp4";
 	$title = "Os Simpsons";
         return view('video/index')->with(compact('video', 'mime', 'title'));
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,14 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 Route::get('/video','VideoController@index');
+Route::get('/video/{filename}', function ($filename) {
+    // Pasta dos videos.
+    $videosDir = base_path('resources/assets/videos');
+    if (file_exists($filePath = $videosDir."/".$filename)) {
+        $stream = new \App\VideoStream($filePath);
+        return response()->stream(function() use ($stream) {
+            $stream->start();
+        });
+    }
+    return response("File doesn't exists", 404);
+});
